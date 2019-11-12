ae9c9d755a40329205fccb5f03db70d274f0c61d
christides11 <christides11@hotmail.com>
2019-11-12 13:25:24 -0500

    Printing the prompt to enter characters.

--- project2.s
+++ project2.s
@@ -3,3 +3,12 @@
 	sr1: .asciiz "Enter up to 1000 characters: "
 	newLine: .asciiz "\n"
 	userNumber: .space 1001 #1000 characters
+	
+.text # Instructions section, goes in text segment.
+
+main:
+	lw $t9, base # Store our base in $t9
+	# PRINT PROMPT #
+	li $v0, 4 # System call to print a string.
+	la $a0, sr1 # Load string to be printed.
+	syscall # Print string.
