2302edcc19f6b0b5aa1a2734442cce780ef3a0fe
christides11 <christides11@hotmail.com>
2019-11-12 13:26:43 -0500

    Reading user input.

--- project2.s
+++ project2.s
@@ -8,7 +8,22 @@
 
 main:
 	lw $t9, base # Store our base in $t9
+	
 	# PRINT PROMPT #
 	li $v0, 4 # System call to print a string.
 	la $a0, sr1 # Load string to be printed.
 	syscall # Print string.
+	
+	# READ USER INPUT #
+	li $v0, 8 # System call for taking in input.
+	la $a0, userNumber # Where the string is saved.
+	li $a1, 1001 # Max number of characters to read.
+	syscall
+	
+	# END OF PROGRAM #
+endProgram:
+	li $v0, 4 # Printing new line.
+	la $a0, newLine
+	syscall 
+	li $v0, 10 # Exit program system call.
+	syscall
