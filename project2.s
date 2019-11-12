cee2f937e7d85ac0beb709e269cf681a61363ee1
christides11 <christides11@hotmail.com>
2019-11-12 14:48:38 -0500

    Work on the remove leading loop, renamed userNumber to userString, added string for our reconstructed string.

--- project2.s
+++ project2.s
@@ -2,7 +2,8 @@
 	base: .word 33
 	sr1: .asciiz "Enter up to 1000 characters: "
 	newLine: .asciiz "\n"
-	userNumber: .space 1001 #1000 characters
+	userString: .space 1001 #1000 characters
+	newString: .space 1001 
 	
 .text # Instructions section, goes in text segment.
 
@@ -16,7 +17,7 @@ main:
 	
 	# READ USER INPUT #
 	li $v0, 8 # System call for taking in input.
-	la $a0, userNumber # Where the string is saved.
+	la $a0, userString # Where the string is saved.
 	li $a1, 1001 # Max number of characters to read.
 	syscall
 	
@@ -31,8 +32,11 @@ endProgram:
 
 # REMOVE LEADING SPACES #
 removeLeading:
-	la $s0, userNumber # The address of the string the user entered.
+	la $s0, userString # The address of the string the user entered.
 	add $t0, $t0, $zero # $t0 will iterate over each character.
 rLLoop:
 	add $s1, $s0, $t0 # message[i]
 	lb $s2, 0($s1) # Load the character at message[i] into $s2.
+	beq $s2, 0, rLLoopEnd # End of string, exit out.
+rLLoopEnd:
+	jr $ra # Return to where we were in the main loop.
